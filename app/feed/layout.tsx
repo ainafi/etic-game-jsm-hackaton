bd4cc2a8c54e7a06bb2a79958aaea13482023fda
ainafi <razanaminoainafinaritra@gmail.com>
2024-12-09 22:06:53 +0300

    debug layout and debug tanstack query

--- app/feed/layout.tsx
+++ app/feed/layout.tsx
@@ -1,21 +1,36 @@
+"use client"
+
+import { useEffect, useState } from "react";
 import Sidebar from "@/components/shared/Sidebar";
-// import { fetchData } from "@/lib/fetchdata";
 import ProtectedRoute from "@/middleware/ProtectedRoute";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
-export default function FeedLayout({children,}:{children: React.ReactNode;}) {
-    return (
+export default function FeedLayout({ children }: { children: React.ReactNode }) {
+  const [clientReady, setClientReady] = useState(false);
+
+  useEffect(() => {
+    // This will run only on the client-side
+    setClientReady(true);
+  }, []);
+
+  if (!clientReady) {
+    return null; // Optionally, you can return a loading spinner or skeleton screen
+  }
+
+  return (
+    <QueryClientProvider client={queryClient}>
       <ProtectedRoute>
         <main className="bg-dark flex min-h-screen w-full flex-col lg:flex-row">
-          <Sidebar/>
+          <Sidebar />
           <div className="mt-16 flex-1 overflow-auto py-8 lg:mt-0 lg:max-h-screen lg:py-10">
             <div className="max-w-5xl mx-auto px-5 md:px-10 w-full text-dark-400 p-16-regular">
               {children}
-
             </div>
           </div>
         </main>
-
       </ProtectedRoute>
-    );
-  }
+    </QueryClientProvider>
+  );
+}
--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,9 +1,26 @@
 "use client"
-
+import { fetchData } from '@/lib/fetchdata'
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
+interface Igenre{
+  id:number
+  name:string
+}
 const Sidebar = () => {
+  const {isLoading,data,error}=useQuery({
+    queryKey:["genres"],
+    queryFn:()=>fetchData("genre/movie/list")
+  })
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>Error: {error.message}</div>
   return (
-    <div>Sidebar</div>
+    <aside className='my-10'>
+      {data.genres.map((genre: Igenre) => (
+        <ul className=''  key={genre.id}>
+          <li className='text-white text-md border px-4 flex items-center justify-center my-3 '>{genre.name}</li>
+        </ul>
+      ))}
+    </aside>
   )
 }
 
