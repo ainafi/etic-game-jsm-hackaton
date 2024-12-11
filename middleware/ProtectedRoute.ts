17e084bb9d91d8ca8137d06b46d0282c8e3b6345
ainafi <razanaminoainafinaritra@gmail.com>
2024-12-12 02:22:18 +0300

    debug auth

--- middleware/ProtectedRoute.ts
+++ middleware/ProtectedRoute.ts
@@ -1,31 +1,44 @@
-"use client"
-// import { Loader } from 'lucide-react';
-import { useEffect } from "react"
-import { useRouter } from "next/navigation"
-import useAuthStore from "@/store/useAuth"
-import { getCurrentUser } from "@/lib/auth"
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import useAuthStore from "@/store/useAuth";
+import { getCurrentUser, logout } from "@/lib/auth";
 import { toast } from '@/hooks/use-toast';
+import useNetworkStatus from '@/hooks/useNetworkStatus';
 
-const ProtectedRoute=({children}:{children: React.ReactNode;})=>{
-    const router=useRouter()
-    const user=useAuthStore((state)=>state.user)
-    useEffect(() => {
-      const checkUser=async()=>{
-        try {
-            await getCurrentUser()
-        } catch (error) {
-            router.push('/sign-in')
-            return toast({title:"please connect to your account",variant:"destructive"})
-            console.log(error)
-        }
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+  const user = useAuthStore((state) => state.user);
+  const isOnline = useNetworkStatus();
+
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        await getCurrentUser();
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error:any) {
+        console.log(error)
+        router.push('/sign-in');
+        return toast({ title: "Please connect to your account", variant: "destructive" });
       }
-      checkUser()
-    }, [router])
-    if (!user) {
-        return null
-    }
-    return children
-    
-}
+    };
 
-export default ProtectedRoute
+    checkUser();
+  }, [router]);
+
+  useEffect(() => {
+    if (!isOnline) {
+      logout();
+      router.push('/sign-in');
+      toast({ title: "You have been logged out due to network disconnection", variant: "destructive" });
+    }
+  }, [isOnline, router]);
+
+  if (!user) {
+    return null;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
