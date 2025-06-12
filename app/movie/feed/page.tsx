8a7d3386e96192ecd448052df2a02a3d0baccb2b
ainafi <razanaminoainafinaritra@gmail.com>
2025-06-13 00:06:11 +0300

    refact: improve code formatting and structure in Feed and AppSidebar components

--- app/movie/feed/page.tsx
+++ app/movie/feed/page.tsx
@@ -1,29 +1,30 @@
 "use client"
-import React, { useRef, useCallback } from 'react'
-import { useInfiniteQuery } from '@tanstack/react-query'
-import { fetchData } from '@/lib/fetchdata'
-import CardFeed from '@/components/shared/CardFeed'
+
+import React, { useRef, useCallback } from "react"
+import { useInfiniteQuery } from "@tanstack/react-query"
+import { fetchData } from "@/lib/fetchdata"
+import CardFeed from "@/components/shared/CardFeed"
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { Discover } from '@/constant'
-import { Button } from '@/components/ui/button'
-import { ChevronDown } from 'lucide-react'
-import useGenre from '@/store/useGenre'
+import { Discover } from "@/constant"
+import { Button } from "@/components/ui/button"
+import { ChevronDown } from "lucide-react"
+import useGenre from "@/store/useGenre"
 
 interface Imovie {
-  id: number,
-  poster_path: string,
-  media_type: string,
-  title: string |undefined,
+  id: number
+  poster_path: string
+  media_type: string
+  title: string | undefined
   name?: string
 }
 
 const Feed = () => {
-  const {genre,setgenre}=useGenre()
+  const { genre, setgenre } = useGenre()
   const {
     data,
     isLoading,
@@ -34,12 +35,13 @@ const Feed = () => {
     status,
   } = useInfiniteQuery({
     queryKey: ["discover", genre],
-    queryFn: ({ pageParam = 1 }) => fetchData(`discover/${genre}`, {
-      include_adult: 'false',
-      sort_by: 'popularity.desc',
-      page: pageParam,
-      language: 'en-US'
-    }),
+    queryFn: ({ pageParam = 1 }) =>
+      fetchData(`discover/${genre}`, {
+        include_adult: "false",
+        sort_by: "popularity.desc",
+        page: pageParam,
+        language: "en-US",
+      }),
     getNextPageParam: (lastPage) => {
       if (lastPage.page < lastPage.total_pages) {
         return lastPage.page + 1
@@ -47,9 +49,10 @@ const Feed = () => {
         return undefined
       }
     },
-    initialPageParam: 1
+    initialPageParam: 1,
   })
   
+
   const observer = useRef<IntersectionObserver | null>(null)
   const lastMovieElementRef = useCallback(
     (node: HTMLDivElement) => {
@@ -67,31 +70,42 @@ const Feed = () => {
 
   if (isLoading) {
     return (
-      <div className='flex flex-wrap gap-4'>
+      <div className="flex flex-wrap gap-4">
         {Array.from({ length: 10 }).map((_, index) => (
-          <CardFeed isLoading key={index} isSkeleton id={0} media_type={''} title={''} price={0} />
+          <CardFeed
+            isLoading
+            key={index}
+            isSkeleton
+            id={0}
+            media_type={""}
+            title={""}
+            price={0}
+          />
         ))}
       </div>
     )
   }
 
-  if (status === 'error') {
+  if (status === "error") {
     return <div>Error: {error.message}</div>
   }
-  console.log(data)
+
   return (
     <div>
       <div>
-        <Button  className='md:w-[100px] mt-5 w-full bg-transparent border-white border capitalize'>
-          <DropdownMenu >
-            <DropdownMenuTrigger className='w-full border-none'>
-              <div className='flex items-center  justify-between capitalize'>
-                {genre} <ChevronDown /> 
+        <Button className="md:w-[100px] mt-5 w-full bg-transparent border-white border capitalize">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="w-full border-none">
+              <div className="flex items-center justify-between capitalize">
+                {genre} <ChevronDown />
               </div>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
               {Discover.map((item) => (
-                <DropdownMenuItem key={item.id} onClick={() => setgenre(item.name)}>
+                <DropdownMenuItem
+                  key={item.id}
+                  onClick={() => setgenre(item.name)}
+                >
                   {item.name}
                 </DropdownMenuItem>
               ))}
@@ -99,25 +113,30 @@ const Feed = () => {
           </DropdownMenu>
         </Button>
       </div>
-      <div className='flex flex-wrap gap-4 justify-center md:justify-normal'>
+      <div className="flex flex-wrap gap-4 justify-center md:justify-normal">
         {data?.pages.map((page, pageIndex) =>
           page.results.map((movie: Imovie, movieIndex: number) => {
-            const isLastMovie = pageIndex === data.pages.length - 1 && movieIndex === page.results.length - 1
-            
+            const isLastMovie =
+              pageIndex === data.pages.length - 1 &&
+              movieIndex === page.results.length - 1
+
             return (
-              <>
-                <CardFeed
-                  
-                  media_type={movie.media_type}
-                  ref={isLastMovie ? lastMovieElementRef : null}
-                  key={movie.id}
-                  isLoading={isLoading}
-                  poster_path={movie.poster_path} id={movie.id}title={movie.title?.toString() ?? movie.name ?? ''} price={0}/>
-              </>
+              <CardFeed
+                media_type={movie.media_type}
+                ref={isLastMovie ? lastMovieElementRef : null}
+                key={movie.id}
+                isLoading={isLoading}
+                poster_path={movie.poster_path}
+                id={movie.id}
+                title={movie.title?.toString() ?? movie.name ?? ""}
+                price={0}
+              />
             )
           })
         )}
-        {isFetchingNextPage && <div className='text-center'>Loading more...</div>}
+        {isFetchingNextPage && (
+          <div className="text-center">Loading more...</div>
+        )}
       </div>
     </div>
   )
